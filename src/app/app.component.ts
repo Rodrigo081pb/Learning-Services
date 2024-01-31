cc52e22be635657b28bff44193e04f0b6eeed669
Rodrigo <kauarodrigo1193@gmail.com>
2024-01-30 21:48:39 -0300

    Seu commit message aqui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Aprendendo Services</h1>
-    <router-outlet></router-outlet>
-    <app-card></app-card>
+  <h1>Aula de Services</h1>
+  <app-card></app-card>
+  <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Services';
+  title = 'Pokehere';
 }
